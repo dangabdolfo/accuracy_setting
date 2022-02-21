ba4996eeaac25916916ab5b686fe6a3ca6c4cd26
daniel <daniel.gandolfo@dreaminzzz.com>
2022-02-21 16:49:08 +0100

    Version 2.2.2
    Fixing ios version

--- ios/Classes/SwiftAccuracySettingPlugin.swift
+++ ios/Classes/SwiftAccuracySettingPlugin.swift
@@ -2,6 +2,7 @@ import Flutter
 import Foundation
 import UIKit
 import CoreLocation
+import SystemConfiguration.CaptiveNetwork
 
 public class SwiftAccuracySettingPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
